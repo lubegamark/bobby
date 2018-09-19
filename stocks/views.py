68d9a7a292a2c70e0e35b606e03a85497353477a
Lubega Mark <lubegamark@gmail.com>
2018-09-19 23:24:03 +0300

    Add view for getting demo stocks

--- stocks/views.py
+++ stocks/views.py
@@ -1,3 +1,20 @@
+import requests
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def get_stock(request, symbol):
+    BASE_URL = 'https://www.alphavantage.co/query'
+    function = 'TIME_SERIES_DAILY'
+    api_key = 'demo'
+    url = '{}?function={}&symbol={}&apikey={}'.format(
+        BASE_URL,
+        function,
+        symbol,
+        api_key)
+
+    r = requests.get(url=url)
+
+    html = "<html><body>The data is <br/>{}.</body></html>".format(r.json())
+    return HttpResponse(html)
