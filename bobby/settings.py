5d0b07fef8332f68beb1cb1f3f5af53ec697f361
Lubega Mark <lubegamark@gmail.com>
2018-09-20 10:24:53 +0300

    Add static files

--- bobby/settings.py
+++ bobby/settings.py
@@ -121,6 +121,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static_files"),
+]
+
 ALPHA_ADVANTAGE_API_KEY = 'demo'
 
 OPEN_WEATHER_API_KEY = 'demo'
