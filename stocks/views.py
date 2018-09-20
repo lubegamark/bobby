64cf79ade1deaf616e39a44fd3e1f5c70a148110
Lubega Mark <lubegamark@gmail.com>
2018-09-21 00:17:54 +0300

    Convert get methods to non views and use stock data

--- stocks/views.py
+++ stocks/views.py
@@ -1,9 +1,10 @@
 import requests
+import datetime
 from django.http import HttpResponse
 from django.conf import settings
 from django.shortcuts import render
 
-def get_stock(request, symbol):
+def get_stock(symbol):
     BASE_URL = 'https://www.alphavantage.co/query'
     function = 'TIME_SERIES_DAILY'
     api_key = settings.ALPHA_ADVANTAGE_API_KEY
@@ -15,10 +16,10 @@ def get_stock(request, symbol):
 
     r = requests.get(url=url)
 
-    html = "<html><body>The data is <br/>{}.</body></html>".format(r.json())
-    return HttpResponse(html)
+    return r.json()
 
-def get_weather(request, town):
+
+def get_weather(town):
     BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
     api_key = settings.OPEN_WEATHER_API_KEY
     url = '{}?q={}&APPID={}'.format(
@@ -28,22 +29,22 @@ def get_weather(request, town):
 
     r = requests.get(url=url)
 
-    html = "<html><body>The data is <br/>{}.</body></html>".format(r.json())
-    return HttpResponse(html)
+    return r.json()
 
 def index(request):
     context = {}
     if request.method == 'POST':
-        print(request.POST)
+        stock = get_stock(request.POST['symbol'])
         symbol = request.POST['symbol']
-        city = request.POST['city']
+        city = get_weather(request.POST['city'])
         currency = request.POST['currency']
+        print(stock['Time Series (Daily)'][datetime.datetime.now().strftime("%Y-%m-%d")])
         context = {
             "stock": {
-                "price": symbol,
+                "price": stock['Time Series (Daily)'][datetime.datetime.now().strftime("%Y-%m-%d")]['1. open'],
                 "symbol": symbol,
-                "high": symbol,
-                "low": symbol
+                "high": stock['Time Series (Daily)'][datetime.datetime.now().strftime("%Y-%m-%d")]['2. high'],
+                "low": stock['Time Series (Daily)'][datetime.datetime.now().strftime("%Y-%m-%d")]['3. low']
                 },
             "weather": {
                 "city": city,
